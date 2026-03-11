dfd3de6609352c84dcf4ef7270554ea21ccbeb8e
Krishna Jha <117565242+1am-krishna-2407@users.noreply.github.com>
2026-03-11 21:17:46 +0530

    Refactor BASE_DIR assignment in skill_gap.py

--- resume_ai_analyzer/modules/skill_gap.py
+++ resume_ai_analyzer/modules/skill_gap.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def find_skill_gap(role, skills):
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     file_path = os.path.join(BASE_DIR, "data", "job_roles.json")
     with open(file_path,"r") as f:
        
